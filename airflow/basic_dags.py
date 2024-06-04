a1dc9615f2e7352719a7d74ffecec32eee0e6397
dharana <dharana7723@gmail.com>
2024-06-04 16:10:23 +0900

    feat: add postgres hook and select all

--- airflow/basic_dags.py
+++ airflow/basic_dags.py
@@ -1,13 +1,20 @@
+from datetime import datetime
+
+import pendulum
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 kst = pendulum.timezone("Asia/Seoul")
 
+POSTGRES_CONN_ID = "postgres_default"
+
 default_args = {
     "owner": "basic_dags",
     "email": ["dharana7723@gmail.com"],
-    "email_on_failture": False
+    "email_on_failure": False
 }
 
 with DAG(
@@ -22,6 +29,16 @@ with DAG(
     def print_hello():
         print("Welcome to airflow test")
 
+    def get_data_from_postgres():
+        pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+        connection = pg_hook.get_conn()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM test_table")
+        result = cursor.fetchall()
+        print(result)
+        cursor.close()
+        connection.close()
+
     t1 = DummyOperator(
         task_id="dummy_task_id",
         retries=5,
@@ -32,6 +49,16 @@ with DAG(
         python_callable=print_hello
     )
 
-    t1 >> t2
+    t3 = PythonOperator(
+        task_id="get_data_from_postgres",
+        python_callable=get_data_from_postgres,
+        op_kwargs={
+            "copy_sql": "COPY(SELECT * FROM CUSTOMER \
+            WHERE first_name ='john' ) TO \
+            STDOUT WITH CSV HEADER"
+        }
+    )
 
-    
+    t1 >> t2
+    t3
+
